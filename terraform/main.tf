337c1cc0a713ee400fa307f732caf86f6a8c1d51
Suleman-xel <muhammad.suleman@camp2.tkxel.com>
2024-07-09 19:16:46 +0500

    Setting Ref

--- terraform/main.tf
+++ terraform/main.tf
@@ -57,7 +57,7 @@ data "aws_ami" "ubuntu" {
 
 
 resource "aws_instance" "DevOps_instance" {
-  ami           = aws.ami.ubuntu.id
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.DevOps_subnet.id
 
